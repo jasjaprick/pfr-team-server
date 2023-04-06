79418251ce5220597cdf3e228c991404991ef1fc
gridwork-nl <info@gridwork.nl>
2023-04-06 12:48:21 +0200

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const usersRouter = require("./Router");
 
 dotenv.config();
